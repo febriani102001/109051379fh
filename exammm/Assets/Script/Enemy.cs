a8f154f8ae3f76fc37218e8fa1cef0c6f895c9e3
febriani102001 <109051379@live.asia.edu.tw>
2022-11-09 02:00:08 +0800

    update

--- exammm/Assets/Script/Enemy.cs
+++ exammm/Assets/Script/Enemy.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+
+    
     [SerializeField] float health, maxHealth = 1f;
+
+    [SerializeField] private float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }   
 
     private void Start()
     {
         health = maxHealth;
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void Update()
+    {
+      if(target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
     }
 
     public void TakeDamage(float damageAmount)
@@ -19,7 +50,7 @@ public class Enemy : MonoBehaviour
         if(health <= 0)
         {
             Destroy(gameObject);
-           
+         
         }    
     }
 
